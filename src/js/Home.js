0348e827219fb4c0b04e1f7c31b98643ada0aed4
Mark Wen <markwenguojie94@gmail.com>
2017-07-31 22:07:43 -0500

    Partly building home page and navbar

--- src/js/Home.js
+++ src/js/Home.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class Home extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Home</h1>
+			<div className="home">
+				<div className="home-landing">
+					<div className="home-title">
+						<h1>Mark Wen</h1>
+						<p>Web and Android Developer</p>
+					</div>
+					<div className="home-logos">
+
+					</div>
+				</div>
+
+				<div className="home-cards">
+
+				</div>
 			</div>
 		);
 	}
--- src/js/components/NavigationBar.js
+++ src/js/components/NavigationBar.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
+
+import BlueLogoTransparentBG from '../../img/logo/BlueLogoTransparentBG.png'
+import WhiteLogoTransparentBG from '../../img/logo/WhiteLogoTransparentBG.png'
 
 /**
 * Top navbar, change transparency based on scroll
 */
 class NavigationBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+
+	/**
+	* On dropdown toggle click
+	*/
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+
+	/**
+	* On navbar item click
+	* swap background image
+	*/
+	onNavItemClick(background) {
+		document.body.className = background;
+	}
+
 	render() {
 		return (
-			<navbar className="navbar">
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/Resume">Resume</Link></li>
-					<li><Link to="/Portfolio">Portfolio</Link></li>
-					<li><Link to="/Contact">Contact</Link></li>
-				</ul>
-			</navbar>
+			<Navbar color="faded" light toggleable>
+				{/* Collapse button */}
+				<NavbarToggler right onClick={this.toggle} />
+				{/* Logo */}
+				<Link to="/"><img className="navbar-logo" src={WhiteLogoTransparentBG} alt="MW Logo"/></Link>
+				{/* Links */}
+				<Collapse isOpen={this.state.isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem><Link to="/" onClick={this.onNavItemClick("home-background")}>Home</Link></NavItem>
+						<NavItem><Link to="/Resume" onClick={this.onNavItemClick("resume-background")}>Resume</Link></NavItem>
+						<NavItem><Link to="/Portfolio" onClick={this.onNavItemClick("portfolio-background")}>Portfolio</Link></NavItem>
+						<NavItem><Link to="/Contact" onClick={this.onNavItemClick("contact-background")}>Contact</Link></NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
 		);
 	}
 }
