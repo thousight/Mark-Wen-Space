34112db2af21d252af89c1fae5713225b83b3497
Mark Wen <markwenguojie94@gmail.com>
2017-08-06 02:06:12 -0400

    Revert "Release 0.7"
    This reverts commit 9ada4767e4ea0573eacaef4eecfbb99b6a2a00e4.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 	render() {
 		return (
 			<div id="root-view">
-				{/* <Sidebar /> */}
+				<Sidebar />
 				<NavigationBar />
 
 				<Route exact path="/" component={Home} />
