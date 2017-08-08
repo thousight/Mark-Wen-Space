62890ee6840d96d1db82b4365f799ebeb9c180f2
Mark Wen <markwenguojie94@gmail.com>
2017-08-07 23:53:54 -0400

    Finish contact info card

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Recaptcha from 'react-recaptcha';
+import { Row, Col } from 'react-bootstrap';
 
+import envelope from '../img/icons/envelope.svg';
+import home from '../img/icons/home.svg';
+import phone from '../img/icons/phone.svg';
+
+/**
+* Static Contact page, able to send email to Mark
+*/
 class Contact extends Component {
-
-	componentDidMount() {
-		this.sendEmail();
-	}
 
 	sendEmail() {
 		axios.post('http://mark-wen-space-v3-server.herokuapp.com/sendEmail', {
@@ -24,7 +29,45 @@ class Contact extends Component {
 	render() {
 		return (
 			<div className="contact">
-				<h1>Contact</h1>
+				<div className="contact-title">
+					<h1>Contact</h1>
+				</div>
+
+				<div className="container contact-content">
+					<Row>
+						<Col xs={12} sm={5}>
+							<div className="card contact-info">
+								<div className="contact-info-line">
+									<img className="contact-info-icon" alt="Email" src={envelope} />
+									<a href="mailto:markwenguojie94@gmail.com">markwenguojie94@gmail.com</a>
+								</div>
+								<div className="contact-info-line">
+									<img className="contact-info-icon" alt="Phone" src={phone} />
+									<a href="tel:510-505-4398">(510) 505-4398</a>
+								</div>
+								<div className="contact-info-line">
+									<img className="contact-info-icon" alt="Address" src={home} />
+									<div className="contact-info-line-address">
+										<a href="https://goo.gl/maps/f843LXUZbTL2" target="_blank" rel="noopener noreferrer">
+											2120 McCormick Rd. Apt. 721b West Lafayette, IN 47906
+										</a>
+									</div>
+								</div>
+							</div>
+						</Col>
+
+						<Col xs={12} sm={7}>
+							<div className="card contact-email">
+								<Recaptcha
+									className="contact-recaptcha"
+									siteKey="6LcbFywUAAAAAPcecjPRbVVqzaR4vBQUqVRihzs_"
+									onloadCallback={this.sendEmail}
+									type="image"
+									/>
+							</div>
+						</Col>
+					</Row>
+				</div>
 			</div>
 		);
 	}
