214b04b4953d05b4be36470a39e72a29d0f56662
Mark Wen <markwenguojie94@gmail.com>
2017-12-12 22:17:31 -0500

    Added babel-polyfill to try to improve Google SEO

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'babel-polyfill'
 import { Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
 import { Provider } from 'react-redux';
