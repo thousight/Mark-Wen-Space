fee059ce2f87209e65dc01f6cb49f930227835f8
Mark Wen <markwenguojie94@gmail.com>
2018-09-12 20:55:48 -0400

    Added eslint and prettier config to react frontend

--- src/js/components/NavigationBar.jsx
+++ src/js/components/NavigationBar.jsx
@@ -1,197 +1,214 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router-dom'
 import { bindActionCreators } from 'redux'
-import {
-	Navbar,
-	Nav,
-	NavItem,
-	Row,
-	Col,
-} from 'react-bootstrap'
+import { Navbar, Nav, NavItem, Row, Col } from 'react-bootstrap'
 
 import { toggleSidebar, setNavbarCurrentItem } from '../redux/actions'
 
 import BlueLogoTransparentBG from '../../img/logo/BlueLogoTransparentBG.png'
 import WhiteLogoTransparentBG from '../../img/logo/WhiteLogoTransparentBG.png'
 
-
 /**
-* Top navbar, change transparency based on scroll
-*/
+ * Top navbar, change transparency based on scroll
+ */
 class NavigationBar extends Component {
+  handleScroll = this.handleScroll.bind(this)
 
-	handleScroll = this.handleScroll.bind(this)
+  toggleOnClick = this.toggleOnClick.bind(this)
 
-	toggleOnClick = this.toggleOnClick.bind(this)
+  getLinkClassNames = this.getLinkClassNames.bind(this)
 
-	getLinkClassNames = this.getLinkClassNames.bind(this)
+  navItemOnClick = this.navItemOnClick.bind(this)
 
-	navItemOnClick = this.navItemOnClick.bind(this)
+  state = {
+    logo: WhiteLogoTransparentBG,
+    navbarClassName: 'navbar-transparent',
+    navbarBackgroundColor: 'rgba(255, 255, 255, 0)',
+    toggleClassName: 'navbar-toggle-white',
+  }
 
-	state= {
-		logo: WhiteLogoTransparentBG,
-		navbarClassName: 'navbar-transparent',
-		navbarBackgroundColor: 'rgba(255, 255, 255, 0)',
-		toggleClassName: 'navbar-toggle-white',
-	}
+  componentDidMount() {
+    // Select the currently selected nav item based on url
+    this.navItemOnClick(this.props.location.pathname)
 
-	componentDidMount() {
-		// Select the currently selected nav item based on url
-		this.navItemOnClick(this.props.location.pathname)
+    // Add scroll listener
+    window.addEventListener('scroll', this.handleScroll)
+  }
 
-		// Add scroll listener
-		window.addEventListener('scroll', this.handleScroll)
-	}
-
-	componentWillUnmount() {
+  componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll)
-	}
+  }
 
-	/**
-	* Animate navbar background transparency change
-	*/
-	handleScroll() {
-		if (window.scrollY <= 0) {
-			// If user scrolls to the top
-			this.setState({
-				logo: WhiteLogoTransparentBG,
-				navbarClassName: 'navbar-transparent',
-				navbarBackgroundColor: 'rgba(255, 255, 255, 0)',
-				toggleClassName: 'navbar-toggle-white',
-			})
-		} else {
-			this.setState({
-				logo: BlueLogoTransparentBG,
-				navbarClassName: 'navbar-white',
-				navbarBackgroundColor: 'rgba(255, 255, 255, 0.96)',
-				toggleClassName: 'navbar-toggle-dark',
-			})
-		}
+  /**
+   * Animate navbar background transparency change
+   */
+  handleScroll() {
+    if (window.scrollY <= 0) {
+      // If user scrolls to the top
+      this.setState({
+        logo: WhiteLogoTransparentBG,
+        navbarClassName: 'navbar-transparent',
+        navbarBackgroundColor: 'rgba(255, 255, 255, 0)',
+        toggleClassName: 'navbar-toggle-white',
+      })
+    } else {
+      this.setState({
+        logo: BlueLogoTransparentBG,
+        navbarClassName: 'navbar-white',
+        navbarBackgroundColor: 'rgba(255, 255, 255, 0.96)',
+        toggleClassName: 'navbar-toggle-dark',
+      })
+    }
 
-		if (window.scrollY > 0 && window.scrollY <= 30) {
-			// Smoothing background color transition
-				this.setState({ navbarBackgroundColor: `rgba(255, 255, 255, ${window.scrollY / 30 * 0.96})` })
-		}
-	}
+    if (window.scrollY > 0 && window.scrollY <= 30) {
+      // Smoothing background color transition
+      this.setState({
+        navbarBackgroundColor: `rgba(255, 255, 255, ${(window.scrollY / 30) *
+          0.96})`,
+      })
+    }
+  }
 
-	/**
-	* Set active link
-	* @param: address(String)
-	*/
-	getLinkClassNames(address) {
-		return this.props.appSettings.navbarSelectedItem === address ? 'navbar-links-active' : ''
-	}
+  /**
+   * Set active link
+   * @param: address(String)
+   */
+  getLinkClassNames(address) {
+    return this.props.appSettings.navbarSelectedItem === address
+      ? 'navbar-links-active'
+      : ''
+  }
 
-	/**
-	* Make NavItem active and direct user to the page
-	* @param: address(String)
-	*/
-	navItemOnClick(address) {
-		const {
-			setNavbarCurrentItem,
-			history,
-		} = this.props
-		// Scroll to the top of the page
-		window.scrollTo(0, 0)
+  /**
+   * Make NavItem active and direct user to the page
+   * @param: address(String)
+   */
+  navItemOnClick(address) {
+    const { setNavbarCurrentItem, history } = this.props
+    // Scroll to the top of the page
+    window.scrollTo(0, 0)
 
-		if (address === '/') {
-			// Sets active item in Redux and triggers navbar render()
-			setNavbarCurrentItem('Home')
-		} else {
-			// Since address would be '/Resume' format, take out '/'
-			setNavbarCurrentItem(address.substr(1, address.length - 1))
-		}
+    if (address === '/') {
+      // Sets active item in Redux and triggers navbar render()
+      setNavbarCurrentItem('Home')
+    } else {
+      // Since address would be '/Resume' format, take out '/'
+      setNavbarCurrentItem(address.substr(1, address.length - 1))
+    }
 
-		// Navigate user to address
-		history.push(address)
-	}
+    // Navigate user to address
+    history.push(address)
+  }
 
-	/**
-	* Open sidebar on click
-	* @param: event(JS click event object)
-	*/
-	toggleOnClick(event) {
-		event.preventDefault()
-		this.props.toggleSidebar(true)
-	}
+  /**
+   * Open sidebar on click
+   * @param: event(JS click event object)
+   */
+  toggleOnClick(event) {
+    event.preventDefault()
+    this.props.toggleSidebar(true)
+  }
 
-	render() {
-		const {
-			logo,
-			navbarClassName,
-			navbarBackgroundColor,
-			toggleClassName,
-		} = this.state
+  render() {
+    const {
+      logo,
+      navbarClassName,
+      navbarBackgroundColor,
+      toggleClassName,
+    } = this.state
 
-		return (
-			<Navbar
-				collapseOnSelect
-				className={navbarClassName}
-				style={{ backgroundColor: navbarBackgroundColor }}
-			>
-				<Row>
-					<Col xs={12} md={10} mdOffset={1} lg={12} lgOffset={0}>
-						<Navbar.Header>
-							{/* Logo */}
-							<img
-								className="navbar-logo"
-								src={logo}
-								alt="MW Logo"
-								onClick={() => {this.navItemOnClick("/")}}/>
+    return (
+      <Navbar
+        collapseOnSelect
+        className={navbarClassName}
+        style={{ backgroundColor: navbarBackgroundColor }}
+      >
+        <Row>
+          <Col xs={12} md={10} mdOffset={1} lg={12} lgOffset={0}>
+            <Navbar.Header>
+              {/* Logo */}
+              <img
+                className="navbar-logo"
+                src={logo}
+                alt="MW Logo"
+                onClick={() => {
+                  this.navItemOnClick('/')
+                }}
+              />
 
-							{/* Toggle */}
-							<a className={`navbar-toggle ${toggleClassName}`} onClick={this.toggleOnClick}>
-								<span className="icon-bar" />
-								<span className="icon-bar" />
-								<span className="icon-bar" />
-							</a>
-						</Navbar.Header>
+              {/* Toggle */}
+              <a
+                className={`navbar-toggle ${toggleClassName}`}
+                onClick={this.toggleOnClick}
+              >
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+              </a>
+            </Navbar.Header>
 
-						<Nav pullRight>
-							<NavItem
-								className={this.getLinkClassNames('Home')}
-								eventKey={1}
-								onClick={() => {this.navItemOnClick('/')}}
-							>
-								Home
-							</NavItem>
-							<NavItem
-								className={this.getLinkClassNames('Resume')}
-								eventKey={2}
-								onClick={() => {this.navItemOnClick('/Resume')}}
-							>
-								Resume
-							</NavItem>
-							<NavItem
-								className={this.getLinkClassNames('Portfolio')}
-								eventKey={3}
-								onClick={() => {this.navItemOnClick('/Portfolio')}}
-							>
-								Portfolio
-							</NavItem>
-							<NavItem
-								className={this.getLinkClassNames('Contact')}
-								eventKey={4}
-								onClick={() => {this.navItemOnClick('/Contact')}}
-							>
-								Contact
-							</NavItem>
-						</Nav>
-					</Col>
-				</Row>
-			</Navbar>
-		)
-	}
+            <Nav pullRight>
+              <NavItem
+                className={this.getLinkClassNames('Home')}
+                eventKey={1}
+                onClick={() => {
+                  this.navItemOnClick('/')
+                }}
+              >
+                Home
+              </NavItem>
+              <NavItem
+                className={this.getLinkClassNames('Resume')}
+                eventKey={2}
+                onClick={() => {
+                  this.navItemOnClick('/Resume')
+                }}
+              >
+                Resume
+              </NavItem>
+              <NavItem
+                className={this.getLinkClassNames('Portfolio')}
+                eventKey={3}
+                onClick={() => {
+                  this.navItemOnClick('/Portfolio')
+                }}
+              >
+                Portfolio
+              </NavItem>
+              <NavItem
+                className={this.getLinkClassNames('Contact')}
+                eventKey={4}
+                onClick={() => {
+                  this.navItemOnClick('/Contact')
+                }}
+              >
+                Contact
+              </NavItem>
+            </Nav>
+          </Col>
+        </Row>
+      </Navbar>
+    )
+  }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-	toggleSidebar,
-	setNavbarCurrentItem,
-}, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      toggleSidebar,
+      setNavbarCurrentItem,
+    },
+    dispatch,
+  )
 
 const mapStateToProps = state => ({
-	appSettings: state.appSettings,
+  appSettings: state.appSettings,
 })
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavigationBar))
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(NavigationBar),
+)
--- src/js/components/SendEmailForm.jsx
+++ src/js/components/SendEmailForm.jsx
@@ -1,145 +1,172 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router-dom'
 import { Mutation } from 'react-apollo'
 import Recaptcha from 'react-recaptcha'
 import { Modal } from 'react-bootstrap'
 import { toast } from 'react-toastify'
 
-import { SEND_EMAIL } from '../../js/utils/gql'
+import { SEND_EMAIL } from '../utils/gql'
 
 import navigation from '../../img/icons/navigation.svg'
 
-
 class SendEmailForm extends Component {
-    handleEmailSubmit = this.handleEmailSubmit.bind(this)
+  handleEmailSubmit = this.handleEmailSubmit.bind(this)
 
-    handleSendEmail = this.handleSendEmail.bind(this)
+  handleSendEmail = this.handleSendEmail.bind(this)
 
-    sendEmail = null
+  sendEmail = null
 
-    isEmailResponseToasted = true
+  isEmailResponseToasted = true
 
-    state = {
-        name: '',
-        fromEmail: '',
-        subject: '',
-        message: '',
-        isShowRecaptchaModal: false,
+  state = {
+    name: '',
+    fromEmail: '',
+    subject: '',
+    message: '',
+    isShowRecaptchaModal: false,
+  }
+
+  /**
+   * Change input values
+   */
+  updateEmailInputFields(key, value) {
+    this.setState({ [key]: value })
+  }
+
+  /**
+   * Show modal when submit email if recaptcha is available
+   */
+  handleEmailSubmit(e) {
+    e.preventDefault()
+
+    if (window['recaptcha'] && window['grecaptcha']) {
+      this.setState({ isShowRecaptchaModal: true })
+    } else {
+      this.handleSendEmail()
     }
+  }
 
-	/**
-	* Change input values
-	*/
-	updateEmailInputFields(key, value) {
-		this.setState({ [key]: value })
-	}
+  handleSendEmail() {
+    const { name, fromEmail, subject, message } = this.state
 
-	/**
-	* Show modal when submit email if recaptcha is available
-	*/
-	handleEmailSubmit(e) {
-        e.preventDefault()
-
-		if (window['recaptcha'] && window['grecaptcha']) {
-			this.setState({ isShowRecaptchaModal: true })
-		} else {
-            this.handleSendEmail()
-        }
+    if (this.sendEmail) {
+      this.sendEmail({
+        variables: {
+          name,
+          fromEmail,
+          subject,
+          textBody: message,
+        },
+      })
     }
-    
-    handleSendEmail() {
-        const { name, fromEmail, subject, message } = this.state
+    this.isEmailResponseToasted = false
+    this.setState({ isShowRecaptchaModal: false })
+  }
 
-        if (this.sendEmail) {
-            this.sendEmail({
-                variables: {
-                    name,
-                    fromEmail,
-                    subject,
-                    textBody: message,
-                },
-            })
-        }
-        this.isEmailResponseToasted = false
-        this.setState({ isShowRecaptchaModal: false })
-    }
+  render() {
+    const {
+      name,
+      fromEmail,
+      subject,
+      message,
+      isShowRecaptchaModal,
+    } = this.state
 
-    render() {
-        const {
-            name,
-            fromEmail,
-            subject,
-            message,
-            isShowRecaptchaModal,
-        } = this.state
-        
-        return (
-            <Mutation mutation={SEND_EMAIL}>
-                {(sendEmail, { loading, error, data }) => {
-                    this.sendEmail = sendEmail
+    return (
+      <Mutation mutation={SEND_EMAIL}>
+        {(sendEmail, { loading, error, data }) => {
+          this.sendEmail = sendEmail
 
-                    if (!loading && !this.isEmailResponseToasted && error) {
-                        toast.error(error.message ? error.message : 'Something is wrong when sending email')
-                        this.isEmailResponseToasted = true
-                    }
+          if (!loading && !this.isEmailResponseToasted && error) {
+            toast.error(
+              error.message
+                ? error.message
+                : 'Something is wrong when sending email',
+            )
+            this.isEmailResponseToasted = true
+          }
 
-                    if (!loading && !this.isEmailResponseToasted && data) {
-                        toast('Email successfully sent!')
-                        this.isEmailResponseToasted = true
-                    }
+          if (!loading && !this.isEmailResponseToasted && data) {
+            toast('Email successfully sent!')
+            this.isEmailResponseToasted = true
+          }
 
-                    return (
-                        <div className="card contact-email">
-                            <h4>Shoot me a message!</h4>
-    
-                            <form onSubmit={this.handleEmailSubmit}>
-                                <input className="contact-email-form" 
-                                    value={name}
-                                    onChange={e => this.updateEmailInputFields('name', e.target.value)}
-                                    type="text"
-                                    placeholder="Name" 
-                                    aria-label="Name input field" />
-    
-                                <input className="contact-email-form" 
-                                    value={fromEmail} 
-                                    onChange={e => this.updateEmailInputFields('fromEmail', e.target.value)}
-                                    type="email" 
-                                    placeholder="Email"
-                                    aria-label="Email input field" />
-    
-                                <input className="contact-email-form" 
-                                    value={subject} 
-                                    onChange={e => this.updateEmailInputFields('subject', e.target.value)}
-                                    type="text" 
-                                    placeholder="Subject" 
-                                    aria-label="Subject input field" />
-    
-                                <textarea className="contact-email-form" 
-                                    value={message} 
-                                    onChange={e => this.updateEmailInputFields('message', e.target.value)}
-                                    type="text" 
-                                    placeholder="Message" 
-                                    aria-label="Message input field" />
-    
-                                <div className="contact-email-form-submit-wrapper">
-                                    <button className="contact-email-submit" type="submit">
-                                        <img className="contact-email-submit-icon" alt="submit" src={navigation} />
-                                        <p>{loading ? 'Loading' : 'Submit'}</p>
-                                    </button>
-                                </div>
-                            </form>
-                            <Modal show={isShowRecaptchaModal} onHide={() => this.setState({ isShowRecaptchaModal: false })}>
-                                <Recaptcha
-                                    className="recaptcha"
-                                    sitekey={process.env.REACT_APP_RECAPTCHA}
-                                    verifyCallback={this.handleSendEmail} />
-                            </Modal>
-                        </div>
-                    )
-                }}
-            </Mutation>
-        )
-    }
+          return (
+            <div className="card contact-email">
+              <h4>Shoot me a message!</h4>
+
+              <form onSubmit={this.handleEmailSubmit}>
+                <input
+                  className="contact-email-form"
+                  value={name}
+                  onChange={e =>
+                    this.updateEmailInputFields('name', e.target.value)
+                  }
+                  type="text"
+                  placeholder="Name"
+                  aria-label="Name input field"
+                />
+
+                <input
+                  className="contact-email-form"
+                  value={fromEmail}
+                  onChange={e =>
+                    this.updateEmailInputFields('fromEmail', e.target.value)
+                  }
+                  type="email"
+                  placeholder="Email"
+                  aria-label="Email input field"
+                />
+
+                <input
+                  className="contact-email-form"
+                  value={subject}
+                  onChange={e =>
+                    this.updateEmailInputFields('subject', e.target.value)
+                  }
+                  type="text"
+                  placeholder="Subject"
+                  aria-label="Subject input field"
+                />
+
+                <textarea
+                  className="contact-email-form"
+                  value={message}
+                  onChange={e =>
+                    this.updateEmailInputFields('message', e.target.value)
+                  }
+                  type="text"
+                  placeholder="Message"
+                  aria-label="Message input field"
+                />
+
+                <div className="contact-email-form-submit-wrapper">
+                  <button className="contact-email-submit" type="submit">
+                    <img
+                      className="contact-email-submit-icon"
+                      alt="submit"
+                      src={navigation}
+                    />
+                    <p>{loading ? 'Loading' : 'Submit'}</p>
+                  </button>
+                </div>
+              </form>
+              <Modal
+                show={isShowRecaptchaModal}
+                onHide={() => this.setState({ isShowRecaptchaModal: false })}
+              >
+                <Recaptcha
+                  className="recaptcha"
+                  sitekey={process.env.REACT_APP_RECAPTCHA}
+                  verifyCallback={this.handleSendEmail}
+                />
+              </Modal>
+            </div>
+          )
+        }}
+      </Mutation>
+    )
+  }
 }
 
 export default withRouter(SendEmailForm)
