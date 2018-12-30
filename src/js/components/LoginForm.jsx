bf7d865fcbbe3fa6aa0278d55c179edd02beed61
Mark Wen <markwenguojie94@gmail.com>
2018-12-30 16:43:29 -0500

    Finished login form UI

--- src/js/components/LoginForm.jsx
+++ src/js/components/LoginForm.jsx
@@ -2,15 +2,48 @@ import React, { PureComponent } from 'react'
 import { withRouter } from 'react-router-dom'
 import { Mutation } from 'react-apollo'
 import { Formik, Field } from 'formik'
-import Recaptcha from 'react-recaptcha'
+
+import { LOG_IN } from '../utils/gql'
 
 class LoginForm extends PureComponent {
   render() {
     return (
       <div className="card login-form">
-        <div className="banner-title">
-          <h1>Log In</h1>
-        </div>
+        <Mutation
+          mutation={LOG_IN}
+          update={this.handleSendEmailUpdate}
+          onError={this.handleSendEmailError}
+        >
+          {(handleLogIn, { loading }) => (
+            <Formik onSubmit={handleLogIn}>
+              {({ handleSubmit, values }) => (
+                <div>
+                  <Field
+                    className="contact-email-form"
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    aria-label="Username input field"
+                  />
+                  <Field
+                    className="contact-email-form"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    aria-label="Password input field"
+                  />
+                  <button
+                    className="contact-email-submit"
+                    type="button"
+                    onClick={handleSubmit}
+                  >
+                    {loading ? 'Loading' : 'Log In'}
+                  </button>
+                </div>
+              )}
+            </Formik>
+          )}
+        </Mutation>
       </div>
     )
   }
--- src/js/utils/gql.js
+++ src/js/utils/gql.js
@@ -66,11 +66,27 @@ export const QUERY_ALL_STATIC_CONTENT = gql`
   }
 `
 
-export const SEND_EMAIL = gql `
+export const SEND_EMAIL = gql`
   mutation SendEmail(
-    $name: String!,
-    $fromEmail: String!,
-    $subject: String,
+    $name: String!
+    $fromEmail: String!
+    $subject: String
+    $textBody: String!
+  ) {
+    sendEmail(
+      name: $name
+      fromEmail: $fromEmail
+      subject: $subject
+      textBody: $textBody
+    )
+  }
+`
+
+export const LOG_IN = gql`
+  mutation SendEmail(
+    $name: String!
+    $fromEmail: String!
+    $subject: String
     $textBody: String!
   ) {
     sendEmail(
