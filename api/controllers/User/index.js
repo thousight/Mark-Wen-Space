d6f4f2a84f29781e7b7fef0562826b92f4b498ad
Mark Wen <markwenguojie94@gmail.com>
2019-01-08 21:41:28 -0500

    Added token login API

--- api/controllers/User/index.js
+++ api/controllers/User/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 
 import emailLoginMiddleware from './emailLoginMiddleware'
 import emailSignupController from './emailSignupController'
+import tokenLoginMiddleware from './tokenLoginMiddleware'
 
 import { protectedRoute } from '../../utils/auth'
 
 const router = Router()
 
+router.get('/login/token', tokenLoginMiddleware)
 router.post('/login/email', emailLoginMiddleware)
 router.post('/signup/email', protectedRoute, emailSignupController)
 
